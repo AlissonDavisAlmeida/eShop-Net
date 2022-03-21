124256a7d11c7da3c00b829b230d7241eddcaf25
alisson <alissoneatena1@gmail.com>
2022-03-21 07:24:44 -0300

    Product interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,35 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
 
+interface Product{
+  id : number
+  name : string
+  description : string
+  price: number
+  pictureUrl: string
+  type: string
+  brand: string
+  quantityInStock: number
+}
+
 function App() {
+  
+  const [products, setproducts] = useState<Product[]>([]);
+  
+  useEffect(()=>{
+    fetch("http://localhost:5000/api/Products")
+    .then(retorno=> retorno.json())
+    .then((items) => setproducts(items))
+  }, [])
+
+
   return (
     <div className="App">
-    
+      <ul>
+        {products.map((product)=>(
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
